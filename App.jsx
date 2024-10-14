228defccffa7c1cf028f364bb14aa295c91143e4
Present111 <161811020+Present111@users.noreply.github.com>
2024-10-14 23:11:47 +0700

    Login

---
+++ App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+//import Login from "./Component/Login/Login";
+import Welcome from "./Component/Welcome/Welcome";
+
+function App() {
+  return <Welcome />;
+}
+
+export default App;
---
+++ Component/Login/Login.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native"; // Đảm bảo bạn đã import 'View'
+
+const Login = () => {
+  return (
+    <View>
+      <Text>help</Text>
+    </View>
+  );
+};
+
+export default Login;
