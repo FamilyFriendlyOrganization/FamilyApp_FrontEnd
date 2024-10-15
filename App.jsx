726cd524a04d6b27cb095a2db4ae0d14690c7fa2
Present111 <161811020+Present111@users.noreply.github.com>
2024-10-15 10:27:36 +0700

    Home

--- App.jsx
+++ App.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 //import Login from "./Component/Login/Login";
 import Welcome from "./Component/Welcome/Welcome";
+import Home from "./Component/Home/Home";
 
 function App() {
-  return <Welcome />;
+  return <Home />;
 }
 
 export default App;
